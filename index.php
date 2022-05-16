d21979c0013cc539b974db1e67bad9201a63889a
Aditya Nugraha <adityanugraha795@gmail.com>
2022-05-16 18:31:19 +0800

    up

---
+++ index.php
@@ -0,0 +1,390 @@
+﻿
+<!DOCTYPE html>
+
+<html lang="en">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>E-Learning SMA Negeri 3 Gorontalo</title>
+        <meta content="E-Learning SMA Negeri 3 Gorontalo" name="E-Learning SMA Negeri 3 Gorontalo">
+
+        <!--<meta content="E-Learning SMA Negeri 3 Gorontalo" name="E-Learning SMA Negeri 3 Gorontalo">-->
+
+        <!-- Favicons -->
+        <link href="assets/img/faviconx.png" rel="icon">
+        <link href="assets/img/faviconx.png" rel="favicon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+        <!--<link href="assets/vendor/aos/aos.css" rel="stylesheet">-->
+        <!--<link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">-->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.7.1/swiper-bundle.min.css" rel="stylesheet">
+        <!--<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">-->
+        <!--<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+        <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+        <!--<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">-->
+        <!--<link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
+        <!--<link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/owl.carousel@2.3.4/dist/assets/owl.carousel.min.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/venobox/1.9.3/venobox.min.css" rel="stylesheet">
+        <!--<link href="assets/vendor/venobox/venobox.css" rel="stylesheet">-->
+        <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">-->
+        <!-- Template Main CSS File -->
+        <link href="assets/css/style.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!--        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+        <!-- =======================================================
+        * Template Name: FlexStart - v1.3.0
+        * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
+</head>
+</head> </html>
+
+<body>
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+        <a href="beranda" class="logo d-flex align-items-center">
+            <img src="assets/img/sscasn.png" alt="" class="img-fluid">
+        </a>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto" href="#">Beranda</a></li>
+                
+                <li><a class="getstarted scrollto" href="http://e-lms.smantiggtlo.com:3195">Login</a></li>
+            </ul>
+                </li>
+
+
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
+
+</html>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+    <ol class="carousel-indicators">
+
+        <li data-target="#2" data-slide-to="2" class="active" ></li>
+        <!-- <li data-target="#3" data-slide-to="3" ></li> -->
+        <!-- <li data-target="#4" data-slide-to="4"></li> -->
+        <li data-target="#1" data-slide-to="1"></li>
+
+    </ol>
+    <div class="carousel-inner" role="listbox">
+
+        <div class="carousel-item active ">
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="hero d-flex align-items-center">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 d-flex flex-column justify-content-center">
+                <h1 data-aos="fade-up">Selamat Datang <br> di E-Learning</h1>
+                <h3 data-aos="fade-up">SMA Negeri 3 Gorontalo</h3>
+<!--                <h2 data-aos="fade-up" data-aos-delay="400">Periksa status Verifikasi kamu <br> mulai tanggal 2 Agustus 2021</h2>-->
+<!--                <h4 data-aos="fade-up" data-aos-delay="400">*setiap instansi mungkin akan berbeda waktu pengumuman hasil verifikasinya-->
+<!--                </h4>-->
+               <br><br> <div data-aos="fade-up" data-aos-delay="600">
+                    <div class="text-lg-start">
+                        
+                        &nbsp;<a href="http://e-lms.smantiggtlo.com:3195" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                            <span>Login</span>
+                            <i class="icofont-login"></i>
+                        </a>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-6 hero-img " data-aos="zoom-out" data-aos-delay="10" >
+                <img src="assets/img/sma3-1.jpg" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+
+</section><!-- End Hero -->
+        </div>
+
+        <div class="carousel-item ">
+
+            <section id="hero1" class="hero2 d-flex align-items-center">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 d-flex flex-column justify-content-center">
+<br><br>
+                            <h3 data-aos="fade-up" style="margin-top: 40px">E-Learning SMA Negeri 3 Gorontalo</h3>
+<br>
+                            <!--                <br>-->
+                            <!--                <h2 style="color: #e3022e; font-weight: bold" data-aos="fade-up">Website akan dilakukan maintenance</h2>-->
+                            <!--                <h2 style="color: #e3022e;font-weight: bold" data-aos="fade-up">Mulai jam 00:00 sd 05:00 WIB...!!!</h2>-->
+                            <div class="row">
+                            <div class="col-lg-12 d-flex flex-column justify-content-center">
+                                <h2  style="margin-top: -10px" data-aos="fade-up" data-aos-delay="400">
+                                    
+                             </h2>
+                                <br>
+                                <div class="text-lg-start">
+                                <a href="https://gurupppk.kemdikbud.go.id/seleksi_p3k_tahap_1/index.php/home/hasil" download class="btn btn-danger scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                                    <span></span>
+
+                                </a></div>
+                                </h4>
+                                <br><br>   <br>       <div data-aos="fade-up" data-aos-delay="10">
+                                    <div class="text-lg-start">
+                                        
+
+                                    </div>
+                                </div>
+                            </div>
+<!--                            <div class="col-lg-6" data-aos="fade-up" data-aos-delay="600">-->
+<!--                                <div class="text-center text-lg-start">-->
+<!--                                    &lt;!&ndash;                            <a href="soon" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">&ndash;&gt;-->
+<!--                                    <a href="https://daftar-sscasn.bkn.go.id/login" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">-->
+<!--                                        <span>Login</span>-->
+<!--                                        <i class="bi bi-arrow-right"></i>-->
+<!--                                    </a>-->
+<!--                                </div>-->
+<!--                            </div>-->
+
+                        </div>
+
+<!--                            <div class="row">-->
+<!--                                <div class="col-lg-6 d-flex flex-column justify-content-center">-->
+<!--                                    <h5 style="font-style: italic" data-aos="fade-up" data-aos-delay="400">Daftar instansi yang membuka pendaftaran</h5>-->
+<!--                                </div>-->
+<!--                                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="600">-->
+<!--                                    <div class="text-center text-lg-start">-->
+<!--                                        <a href="https://data-sscasn.bkn.go.id/periode" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">-->
+<!--                                            <span>Cek Disini</span>-->
+<!--                                            <i class="bi bi-arrow-right"></i>-->
+<!--                                        </a>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+
+<!--                            </div>-->
+
+                        </div>
+                        <div class="col-lg-6 hero-img " data-aos="zoom-out" data-aos-delay="200" >
+                            <img src="assets/img/logo.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+            </section><!-- End Hero -->
+
+
+
+        </div>
+
+
+        <a  class="carousel-control-prev" href="#demo" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a  class="carousel-control-next" href="#demo" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
+    </div></div>
+
+<main id="main">
+
+
+<!--        <a class="justify-content-center d-flex">-->
+<!--        <img src="assets/img/g35.png" ></a>-->
+
+
+
+
+    <!-- ======= Features Section ======= -->
+    <section id="featuresok" class="featuresok section-bg">
+        <div class="container">
+
+            <div class="section-title" data-aos="zoom-out">
+
+                <h2>_____</h2>
+            </div>
+            <br><br>
+            <div class="row content">
+                <div class="col-md-5" data-aos="fade-right">
+                    <img src="assets/img/timeline/1.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-4" data-aos="fade-left">
+                    <h3>1. 
+					</h3>
+                    <p class="">
+                        
+                    </p>
+                    <!--            <ul>-->
+                    <!--              <li><i class="icofont-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>-->
+                    <!--              <li><i class="icofont-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>-->
+                    <!--            </ul>-->
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-5 order-1 order-md-2" data-aos="fade-left">
+                    <img src="assets/img/timeline/2.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5 order-2 order-md-1" data-aos="fade-right">
+                    <h3>2. </h3>
+                    <p >
+                        
+                    </p>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-5" data-aos="fade-right">
+                    <img src="assets/img/timeline/3.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5" data-aos="fade-left">
+                    <h3>3. </h3>
+                    <!--            <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>-->
+                    <ul>
+                        <li><i class="icofont-check"></i>
+                          
+					   </li>
+					   
++				   </ul>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-5 order-1 order-md-2" data-aos="fade-left">
+                    <img src="assets/img/timeline/4.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5 order-2 order-md-1" data-aos="fade-right">
+                    <h3>4. 
+                </div>
+            </div>
+            <div class="row content">
+                <div class="col-md-5" data-aos="fade-right">
+                    <img src="assets/img/timeline/5.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5" data-aos="fade-left">
+                    <h3>5. </h3>
+                   
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-5 order-1 order-md-2" data-aos="fade-left">
+                    <img src="assets/img/timeline/6.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5 order-2 order-md-1" data-aos="fade-right">
+                    <h3>6.</h3>
+                   
+                </div>
+            </div>
+
+
+        </div>
+    </section><!-- End Features Section -->
+
+    
+
+</main><!-- End #main -->
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+
+   
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>SMA Negeri 3 Gorontalo</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
+          E-Learning SMA Negeri 3 Gorontalo
+        </div>
+    </div>
+
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+<!--<script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>-->
+<!--<script src="assets/vendor/aos/aos.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" integrity="sha512-A7AYk1fGKX6S2SsHywmPkrnzTZHrgiVT7GcQkLGDe2ev0aWb8zejytzS8wjo7PGEXKqJOrjQ4oORtnimIRZBtw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<!--<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.7.1/swiper-bundle.min.js" integrity="sha512-I8JVRdFyq+bFWHRZzhTL2lyZtzh7FqUoK/0dZ5mMXS86Cz62M86e3TBGihnabBEF0BrKDLKfh+JuvL/y+WmOQA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
+    <!--<script src="assets/vendor/purecounter/purecounter.js"></script>-->
+<!--<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha512-Zq2BOxyhvnRFXu0+WE6ojpZLOU2jdnqbrM1hmVdGzyeCa1DgM3X5Q4A/Is9xA1IkbUeDd7755dNNI/PzSf2Pew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!--<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>-->
+    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+    <!--<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/owl.carousel@2.3.4/dist/owl.carousel.min.js"></script>
+    <!--<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js" integrity="sha512-fHXRw0CXruAoINU11+hgqYvY/PcsOWzmj0QmcSOtjlJcqITbPyypc8cYpidjPurWpCnlB8VKfRwx6PIpASCUkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!--<script src="assets/vendor/counterup/counterup.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha512-d8F1J2kyiRowBB/8/pAWsqUl0wSEOkG5KATkVV4slfblq9VRQ6MyDZVxWl2tWd+mPhuCbpTB4M7uU/x9FlgQ9Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!--<script src="assets/vendor/venobox/venobox.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/venobox/1.9.3/venobox.min.js" integrity="sha512-zBTnX97k269iewUwROiWwO82A6uXO4lcjq0Z4xnvO+qAblC/RMQRUu8fQVKtSFhPNKD5Xzh9PMoZG7+LnmH1Hg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</footer><!-- End Footer -->
+</html>
+
+
+
+<script type="text/javascript">
+
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RW5M6Y730S"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-RW5M6Y730S');
+</script>
+
+</body>
+
+</html>
